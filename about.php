2a1f48897a4f5d99c59bc85e859be82661dc63eb
Marcel Hacker <china.pvpAT@gmx.at>
2020-04-01 11:54:29 +0200

    Add home.php

--- about.php
+++ about.php
@@ -70,12 +70,12 @@ if (!isset($_COOKIE[$cookie_name])) //wenn nicht eingeloggt User.php nicht anzei
 ?>
 <div class="container">
   <div class="row row-cols-1 row-cols-md-2">
-    <div class="card mb-4">
+    <div class="card mb-5">
       <div class="row no-gutters">
-        <div class="col-md-13">
+        <div class="col-md-14">
           <img src="https://i.dietdoctor.com/wp-content/uploads/2018/08/GettyImages-876656828.jpg?auto=compress%2Cformat&w=800&h=533&fit=crop" class="card-img" alt="image">
         </div>
-        <div class="col-md-13">
+        <div class="col-md-14">
           <div class="card-body">
             <h5 class="card-title">Healthy Eating</h5>
             <p class="card-text">Eating a healthy diet is not about strict limitations, staying unrealistically thin,
