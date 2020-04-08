124916b46babbad3ef66c125238497fa33ddb659
Marcel Hacker <china.pvpAT@gmx.at>
2020-04-08 13:22:14 +0200

    Add cart.php

--- cart.php
+++ cart.php
@@ -8,48 +8,40 @@ if (!isset($_SESSION['userId'])) //wenn nicht eingeloggt User.php nicht anzeigen
     time_sleep_until(3);
     header("Location: login.php");
 } else {
-
-    $db = getDB();
-    if (!$db) {
-        die("Error database connection");
+    $userId = $_SESSION['userId'];
+    $user = getCurrentUser($userId);    // User vorhanden?
+    if (!$user) {                   // Falsche User Id ?
+        echo "Error User";
     } else {
-
-        $statement = $db->prepare("SELECT * FROM users WHERE id = :userId");    // User mit id abfragen
-        $result = $statement->execute(array('userId' => $_SESSION['userId']));  // Mithilfe der Sessionvariable
-        $user = $statement->fetch();    // User vorhanden?
-        if (!$user) {                   // Falsche User Id ?
-            echo "Error User";
-        } else {
 ?>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <ul class="nav nav-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="products.php">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn mr-sm-4 btn-outline-dark" href="logout.php">Logout</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0" action="search.php" method="GET">
-                    <input class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="search" id="search">Search</button>
-                </form>
-                </div>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        Cart ()
-                    </li>
-                </ul>
-            </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="products.php">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn mr-sm-4 btn-outline-dark" href="logout.php">Logout</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0" action="search.php" method="GET">
+                <input class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="search" id="search">Search</button>
+            </form>
+            </div>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    Cart ()
+                </li>
+            </ul>
+        </nav>
 <?php
 
-        }
     }
 }
 
