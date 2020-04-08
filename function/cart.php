3cab4b34d6bbec7d1be338b66c68dc0960b4ef91
Marcel Hacker <china.pvpAT@gmx.at>
2020-04-08 13:03:42 +0200

    Add cart.php functions

--- function/cart.php
+++ function/cart.php
@@ -1,7 +1,7 @@
 <?php
 function addProductToCart(int $userId, int $productId)  // Add product to cart
 {
-  $sql = "INSERT INTO cart SET user_id = :userId,product_Id = :productId";
+  $sql = "INSERT INTO cart SET user_id = :userId, product_Id = :productId";
   $statement = getDB()->prepare($sql);
 
   $statement->execute([
@@ -19,4 +19,34 @@ function countProductsInCart(int $userId)  // Count products in cart
   }
   $cartItems = $cartResults->fetchColumn();
   return $cartItems;
+}
+
+function getCartItemsForUserId(int $userId): array // 
+{
+  $sql = "SELECT product_id,title,description,price
+          FROM cart
+          JOIN products ON(cart.product_id = products.id)
+          WHERE user_id = " . $userId;
+  $results = getDb()->query($sql);
+  if ($results === false) {
+    return [];
+  }
+  $found = [];
+  while ($row = $results->fetch()) {
+    $found[] = $row;
+  }
+  return $found;
+}
+
+function getCartSumForUserId(int $userId): int
+{
+  $sql = "SELECT SUM(price * quantity)
+          FROM cart
+          JOIN products ON(cart.product_id = products.id)
+          WHERE user_id = " . $userId;
+  $result = getDb()->query($sql);
+  if ($result === false) {
+    return 0;
+  }
+  return (int) $result->fetchColumn();
 }
