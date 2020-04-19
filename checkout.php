aa58b8e3503dddd36a6f3ff0468e2a5e319612da
Marcel Hacker <china.pvpAT@gmx.at>
2020-04-19 09:47:14 +0200

    Add order

--- checkout.php
+++ checkout.php
@@ -15,187 +15,11 @@ if (!isLoggedIn()) {
         exit;
     } else {
         $countCartItems = countProductsInCart($userId);
-        if ($showCheckout = true) {
+
 ?>
+        <body>
 
-            <body>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-4 order-md-2 mb-4">
-                            <h4 class="d-flex justify-content-between align-items-center mb-3">
-                                <span class="text-muted">Your cart</span>
-                                <span class="badge badge-secondary badge-pill"><?= $countCartItems ?></span>
-                            </h4>
-                            <ul class="list-group mb-3">
-
-                                <?php
-                                $cartSum = getCartSumForUserId($userId);
-                                $cartItems = getCartItemsForUserId($userId);
-                                foreach ($cartItems as $cartItem) :
-                                ?>
-                                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                                        <div>
-                                            <h6 class="my-0"><?= $cartItem["title"] ?></h6>
-                                            <small class="text-muted"><?= $cartItem["description"] ?></small>
-                                        </div>
-                                        <span class="text-muted"><?= $cartItem["price"] ?> €</span>
-                                    </li>
-                                <?php endforeach; ?>
-                                <li class="list-group-item d-flex justify-content-between">
-                                    <span>Total (EURO)</span>
-                                    <strong><?= number_format($cartSum, 2, ",", " ") ?> €</strong>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-md-8 order-md-1">
-                            <h4 class="mb-3">Billing address</h4>
-                            <form class="needs-validation" novalidate action="checkout.php" method="POST">
-                                <div class="row">
-                                    <div class="col-md-6 mb-3">
-                                        <label for="firstName">First name</label>
-                                        <input type="text" class="form-control" name="firstname" id="firstName" placeholder="" value="<?= $user["firstname"] ?>" required>
-                                        <div class="invalid-feedback">
-                                            Valid first name is required.
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label for="lastName">Last name</label>
-                                        <input type="text" class="form-control" name="lastname" id="lastName" placeholder="" value="<?= $user["lastname"] ?>" required>
-                                        <div class="invalid-feedback">
-                                            Valid last name is required.
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="email">Email <span class="text-muted"></span></label>
-                                    <input type="email" class="form-control" name="email" id="email" value="<?= $user["email"] ?>" placeholder="">
-                                    <div class="invalid-feedback">
-                                        Please enter a valid email address for shipping updates.
-                                    </div>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="inputAddress2">Address</label>
-                                    <input type="text" class="form-control" name="address" id="address" value="<?= $user["address"] ?>" placeholder="" required>
-                                    <div class="invalid-feedback">
-                                        Please enter your shipping address.
-                                    </div>
-                                </div>
-                                <div class="mb-1 col-2">
-                                    <label for="houseNumber">Housenumber</label>
-                                    <input type="text" class="form-control" name="housenumber" id="housenumber" value="<?= $user["housenumber"] ?>" placeholder="">
-                                </div>
-
-                                <div class="row p-1">
-                                    <div class="col-md-5 mb-3">
-                                        <label for="country">Country</label>
-                                        <select class="custom-select d-block w-100" id="country" required>
-                                            <option value="<?= $user["country"] ?>"><?= $user["country"] ?></option>
-                                            <option value="austria">Austria</option>
-                                            <option value="united kingdom">United Kingdom</option>
-                                            <option value="china">China</option>
-                                            <option value="united states">United States</option>
-                                            <option value="schwitzerland">Schwitzerland</option>
-                                            <option value="germany">Germany</option>
-                                        </select>
-                                        <div class="invalid-feedback">
-                                            Please select a valid country.
-                                        </div>
-                                    </div>
-
-                                    <div class="col-md-4 mb-3">
-                                        <label for="city">City</label>
-                                        <input type="text" class="form-control" name="city" id="city" value="<?= $user["city"] ?>" placeholder="Guntersdorf" required>
-                                        <div class="invalid-feedback">
-                                            Please provide a valid city.
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3 mb-3">
-                                        <label for="zip">Zip</label>
-                                        <input type="text" class="form-control" id="zip" value="<?= $user["postcode"] ?>" placeholder="" required>
-                                        <div class="invalid-feedback">
-                                            Zip code required.
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr class="mb-4">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="same-address">
-                                    <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-                                </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="save-info">
-                                    <label class="custom-control-label" for="save-info">Save this information for next time</label>
-                                </div>
-                                <hr class="mb-4">
-
-                                <h4 class="mb-3">Payment</h4>
-
-                                <div class="d-block my-3">
-                                    <div class="custom-control custom-radio">
-                                        <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
-                                        <label class="custom-control-label" for="credit">Credit card</label>
-                                    </div>
-                                    <div class="custom-control custom-radio">
-                                        <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                        <label class="custom-control-label" for="debit">Debit card</label>
-                                    </div>
-                                    <div class="custom-control custom-radio">
-                                        <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
-                                        <label class="custom-control-label" for="paypal">Paypal</label>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-6 mb-3">
-                                        <label for="cc-name">Name on card</label>
-                                        <input type="text" class="form-control" id="cc-name" placeholder="" required>
-                                        <small class="text-muted">Full name as displayed on card</small>
-                                        <div class="invalid-feedback">
-                                            Name on card is required
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6 mb-3">
-                                        <label for="cc-number">Credit card number</label>
-                                        <input type="text" class="form-control" id="cc-number" placeholder="" required>
-                                        <div class="invalid-feedback">
-                                            Credit card number is required
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-3 mb-3">
-                                        <label for="cc-expiration">Expiration</label>
-                                        <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
-                                        <div class="invalid-feedback">
-                                            Expiration date required
-                                        </div>
-                                    </div>
-                                    <div class="col-md-3 mb-3">
-                                        <label for="cc-expiration">CVV</label>
-                                        <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
-                                        <div class="invalid-feedback">
-                                            Security code required
-                                        </div>
-                                    </div>
-                                </div>
-                                <hr class="mb-4">
-                                <button class="btn btn-primary btn-lg btn-block" name="order" type="submit">Buy</button>
-                            </form>
-                        </div>
-                    </div>
-
-                    <footer class="my-5 pt-5 text-muted text-center text-small">
-                        <p class="mb-1">&copy; 2020 Brain Food</p>
-                        <ul class="list-inline">
-                            <li class="list-inline-item"><a href="#">Privacy</a></li>
-                            <li class="list-inline-item"><a href="#">Terms</a></li>
-                            <li class="list-inline-item"><a href="#">Support</a></li>
-                        </ul>
-                    </footer>
-                </div>
     <?php
-        }
     }
 }
 $db = getDB();
@@ -242,6 +66,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update firstname<br>";
             $error = true;
+        } else {
+            echo "firstname updated<br>";
         }
     }
     if ($lastname != $user['lastname']) {
@@ -253,6 +79,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update lastname<br>";
             $error = true;
+        } else {
+            echo "Lastname updated<br>";
         }
     }
     if ($address != $user['address']) {
@@ -262,8 +90,10 @@ if (isset($_POST["order"])) {
 
         $result = $statement->execute(array('address' => $address, 'userid' => $user['id']));
         if (!$result) {
-            echo "Error update adrress<br>";
+            echo "Error update address<br>";
             $error = true;
+        } else {
+            echo "Address updated<br>";
         }
     }
     if ($housenumber != $user['housnumber']) {
@@ -275,6 +105,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update housenumber<br>";
             $error = true;
+        } else {
+            echo "Housenumber updated<br>";
         }
     }
     if ($country != $user['country']) {
@@ -286,6 +118,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update country<br>";
             $error = true;
+        } else {
+            echo "Country updated<br>";
         }
     }
     if ($city != $user['city']) {
@@ -297,6 +131,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update city<br>";
             $error = true;
+        } else {
+            echo "City updated<br>";
         }
     }
     if ($postcode != $user['postcode']) {
@@ -308,6 +144,8 @@ if (isset($_POST["order"])) {
         if (!$result) {
             echo "Error update postcode<br>";
             $error = true;
+        } else {
+            echo "Postcode updated<br>";
         }
     }
 
@@ -330,33 +168,41 @@ if (isset($_POST["order"])) {
                     $orderNumber = $orderNumber + 1;    // Increment for another number
                 }
             }
-
+            // Get items from cart in order
             $sql = "INSERT INTO orders (order_no,user_id,product_id,quantity) 
                     VALUES (:order_no,:user_id,:product_id,:quantity)";
             $statement = $db->prepare($sql);
 
-            foreach ($found as $orders) {         // Order all items in cart
+            foreach ($found as $orders) {         // For all items in cart
                 $result = $statement->execute(array(
                     'order_no' => $orderNumber, 'user_id' => $orders['user_id'],
                     'product_id' => $orders['product_id'], 'quantity' => $orders['quantity']
                 ));
+                if ($result) {          // Check for errors and get order number
+                    echo "Product: " . $orders["product_id"] . " ordered<br>";
+                } else {
+                    echo "Error product Id: " . $orders["product_id"] . "from user Id: " . $orders["user_id"] . "<br>";
+                }
             }
+            $sql = "SELECT order_no FROM orders 
+                    WHERE order_no = :ordernumber";
+            $statement = $db->prepare($sql);
+            $ordered = $statement->execute(array(
+                'ordernumber' => $orderNumber
+            ));
 
-
-            if ($result) {          // Check for errors and get order number
-                $statement = $db->prepare("SELECT * FROM orders WHERE user_id = :userid");
-                $result = $statement->execute(array('email' => $email));
-                $userId = $statement->fetch();
-                $_SESSION['userId'] = $userId;      // Sets User Id
-                echo "<label>$email</label></br>";
-                echo "Registered sucessfully!<br>";
+            if ($ordered) {          // Check order number
+                echo "<label>Thank you for your order!</label></br>";
+                echo "<p>Your ordernumber is: #" . $orderNumber . "</p><br>";
                 $showCheckout = false;
-                //header("Location: index.php");
             } else {
-                echo 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+                echo 'Excuse me, something went wrong<br>';
             }
         }
     }
+}
+if ($showCheckout == true) {
+    include_once("template/checkoutPage.php");
 }
     ?>
 
@@ -392,6 +238,6 @@ if (isset($_POST["order"])) {
             }, false);
         })();
     </script>
-            </body>
+        </body>
 
-            </html>
+        </html>
---
+++ template/checkoutPage.php
@@ -0,0 +1,175 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 order-md-2 mb-4">
+            <h4 class="d-flex justify-content-between align-items-center mb-3">
+                <span class="text-muted">Your cart</span>
+                <span class="badge badge-secondary badge-pill"><?= $countCartItems ?></span>
+            </h4>
+            <ul class="list-group mb-3">
+
+                <?php
+                $cartSum = getCartSumForUserId($userId);
+                $cartItems = getCartItemsForUserId($userId);
+                foreach ($cartItems as $cartItem) :
+                ?>
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 class="my-0"><?= $cartItem["title"] ?></h6>
+                            <small class="text-muted"><?= $cartItem["description"] ?></small>
+                        </div>
+                        <span class="text-muted"><?= $cartItem["price"] ?> €</span>
+                    </li>
+                <?php endforeach; ?>
+                <li class="list-group-item d-flex justify-content-between">
+                    <span>Total (EURO)</span>
+                    <strong><?= number_format($cartSum, 2, ",", " ") ?> €</strong>
+                </li>
+            </ul>
+        </div>
+        <div class="col-md-8 order-md-1">
+            <h4 class="mb-3">Billing address</h4>
+            <form class="needs-validation" novalidate action="checkout.php" method="POST">
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="firstName">First name</label>
+                        <input type="text" class="form-control" name="firstname" id="firstName" placeholder="" value="<?= $user["firstname"] ?>" required>
+                        <div class="invalid-feedback">
+                            Valid first name is required.
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="lastName">Last name</label>
+                        <input type="text" class="form-control" name="lastname" id="lastName" placeholder="" value="<?= $user["lastname"] ?>" required>
+                        <div class="invalid-feedback">
+                            Valid last name is required.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="email">Email <span class="text-muted"></span></label>
+                    <input type="email" class="form-control" name="email" id="email" value="<?= $user["email"] ?>" placeholder="">
+                    <div class="invalid-feedback">
+                        Please enter a valid email address for shipping updates.
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="inputAddress2">Address</label>
+                    <input type="text" class="form-control" name="address" id="address" value="<?= $user["address"] ?>" placeholder="" required>
+                    <div class="invalid-feedback">
+                        Please enter your shipping address.
+                    </div>
+                </div>
+                <div class="mb-1 col-2">
+                    <label for="housenumber">Housenumber</label>
+                    <input type="text" class="form-control" name="housenumber" id="housenumber" value="<?= $user["housenumber"] ?>" placeholder="">
+                </div>
+
+                <div class="row p-1">
+                    <div class="col-md-5 mb-3">
+                        <label for="country">Country</label>
+                        <select class="custom-select d-block w-100" id="country" name="country" required>
+                            <option value="<?= $user["country"] ?>"><?= $user["country"] ?></option>
+                            <option value="austria">Austria</option>
+                            <option value="united kingdom">United Kingdom</option>
+                            <option value="china">China</option>
+                            <option value="united states">United States</option>
+                            <option value="schwitzerland">Schwitzerland</option>
+                            <option value="germany">Germany</option>
+                        </select>
+                        <div class="invalid-feedback">
+                            Please select a valid country.
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mb-3">
+                        <label for="city">City</label>
+                        <input type="text" class="form-control" name="city" id="city" value="<?= $user["city"] ?>" placeholder="Guntersdorf" required>
+                        <div class="invalid-feedback">
+                            Please provide a valid city.
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <label for="zip">Zip</label>
+                        <input type="text" class="form-control" name="postcode" id="postcode" value="<?= $user["postcode"] ?>" placeholder="" required>
+                        <div class="invalid-feedback">
+                            Zip code required.
+                        </div>
+                    </div>
+                </div>
+                <hr class="mb-4">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="same-address">
+                    <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="save-info">
+                    <label class="custom-control-label" for="save-info">Save this information for next time</label>
+                </div>
+                <hr class="mb-4">
+
+                <h4 class="mb-3">Payment</h4>
+
+                <div class="d-block my-3">
+                    <div class="custom-control custom-radio">
+                        <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+                        <label class="custom-control-label" for="credit">Credit card</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                        <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required>
+                        <label class="custom-control-label" for="debit">Debit card</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                        <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+                        <label class="custom-control-label" for="paypal">Paypal</label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="cc-name">Name on card</label>
+                        <input type="text" class="form-control" id="cc-name" placeholder="" required>
+                        <small class="text-muted">Full name as displayed on card</small>
+                        <div class="invalid-feedback">
+                            Name on card is required
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="cc-number">Credit card number</label>
+                        <input type="text" class="form-control" id="cc-number" placeholder="" required>
+                        <div class="invalid-feedback">
+                            Credit card number is required
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 mb-3">
+                        <label for="cc-expiration">Expiration</label>
+                        <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+                        <div class="invalid-feedback">
+                            Expiration date required
+                        </div>
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <label for="cc-expiration">CVV</label>
+                        <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+                        <div class="invalid-feedback">
+                            Security code required
+                        </div>
+                    </div>
+                </div>
+                <hr class="mb-4">
+                <button class="btn btn-primary btn-lg btn-block" name="order" type="submit">Buy</button>
+            </form>
+        </div>
+    </div>
+
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1">&copy; 2020 Brain Food</p>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Privacy</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Support</a></li>
+        </ul>
+    </footer>
+</div>
